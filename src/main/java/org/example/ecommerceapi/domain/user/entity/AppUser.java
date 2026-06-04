9f63fed9edb275c9815592755eb832febb035f11
lawliet62 <dlwldn62@gmail.com>
2026-06-05 04:16:38 +0900

    AppUser 엔티티 수정 및 정적 메서드 추가 (생성자 리팩토링, 롬복 적용)

--- src/main/java/org/example/ecommerceapi/domain/user/entity/AppUser.java
+++ src/main/java/org/example/ecommerceapi/domain/user/entity/AppUser.java
@@ -1,28 +1,38 @@
 package org.example.ecommerceapi.domain.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
 @Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AppUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
-    protected AppUser() {
+    private AppUser(String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
     }
+
+    public static AppUser createUser(String email, String encodedPassword) {
+        return new AppUser(email, encodedPassword, Role.USER);
+    }
+
 }
