9ad4d96cee1e80245d18de74d6a004245e084d7c
lawliet62 <dlwldn62@gmail.com>
2026-06-06 06:54:31 +0900

    ProductRequest 클래스에 유효성 검사 어노테이션 추가

--- src/main/java/org/example/ecommerceapi/domain/product/dto/ProductRequest.java
+++ src/main/java/org/example/ecommerceapi/domain/product/dto/ProductRequest.java
@@ -1,10 +1,15 @@
 package org.example.ecommerceapi.domain.product.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.math.BigDecimal;
 
 public record ProductRequest(
-        String name,
-        BigDecimal price,
-        int stock
+        @NotBlank String name,
+        @NotNull @Positive BigDecimal price,
+        @NotNull @PositiveOrZero Integer stock
 ) {
 }
