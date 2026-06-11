8e340ae6222371096ec68c48f95f5713d3a1e3c3
lawliet62 <dlwldn62@gmail.com>
2026-06-12 08:06:17 +0900

    OrderItem 엔티티에 생성 메서드, 유효성 검사 로직 및 subtotal 계산 메서드 추가

--- src/main/java/org/example/ecommerceapi/domain/order/entity/OrderItem.java
+++ src/main/java/org/example/ecommerceapi/domain/order/entity/OrderItem.java
@@ -1,25 +1,80 @@
 package org.example.ecommerceapi.domain.order.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.math.BigDecimal;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class OrderItem {
+
+    private static final int MAX_QUANTITY = 99;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false, length = 100)
     private String productNameSnapshot;
 
+    @Column(nullable = false, precision = 15, scale = 2)
     private BigDecimal priceSnapshot;
 
+    @Column(nullable = false)
     private int quantity;
 
-    protected OrderItem() {
+    private OrderItem(@NonNull Order order, String productNameSnapshot, BigDecimal priceSnapshot, int quantity) {
+        validateProductNameSnapshot(productNameSnapshot);
+        validatePriceSnapshot(priceSnapshot);
+        validateQuantity(quantity);
+
+        this.order = order;
+        this.productNameSnapshot = productNameSnapshot;
+        this.priceSnapshot = priceSnapshot;
+        this.quantity = quantity;
     }
+
+    private OrderItem create(Order order, String productNameSnapshot, BigDecimal priceSnapshot, int quantity) {
+        return new OrderItem(order, productNameSnapshot, priceSnapshot, quantity);
+    }
+
+    public BigDecimal getSubtotal() {
+        return priceSnapshot.multiply(BigDecimal.valueOf(quantity));
+    }
+
+    private static void validateProductNameSnapshot(String productNameSnapshot) {
+        if (productNameSnapshot == null || productNameSnapshot.isBlank()) {
+            throw new IllegalArgumentException("Product Name Snapshot is required");
+        }
+
+        if (productNameSnapshot.length() > 100) {
+            throw new IllegalArgumentException("Product Name Snapshot must be 100 characters or less");
+        }
+    }
+
+    private static void validatePriceSnapshot(BigDecimal priceSnapshot) {
+        if (priceSnapshot == null || priceSnapshot.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Product price must be positive");
+        }
+    }
+
+    private static void validateQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("quantity must be positive");
+        }
+
+        if (quantity > MAX_QUANTITY) {
+            throw new IllegalArgumentException("Quantity must be 99 or less");
+        }
+    }
+
 }
