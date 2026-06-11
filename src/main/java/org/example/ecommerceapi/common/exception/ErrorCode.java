0c3e22aeacf08670e16c3f67c091f55400de4537
lawliet62 <dlwldn62@gmail.com>
2026-06-12 03:27:55 +0900

    ErrorCode에 USER_NOT_FOUND 추가

--- src/main/java/org/example/ecommerceapi/common/exception/ErrorCode.java
+++ src/main/java/org/example/ecommerceapi/common/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
 
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "Email already exists"),
     INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "Invalid email or password"),
-    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "Product not found");
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "Product not found"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found");
 
     private final HttpStatus status;
     private final String message;
