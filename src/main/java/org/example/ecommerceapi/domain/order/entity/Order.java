907280adf767d20c3f14babe6daa3c5a19992ff1
lawliet62 <dlwldn62@gmail.com>
2026-06-12 07:40:14 +0900

    Order 엔티티에 totalAmount 필드 할당 로직 추가
    Order 엔티티에 사용자 및 총액 필드 추가, 생성 메서드 및 유효성 검사 로직 구현

--- src/main/java/org/example/ecommerceapi/domain/order/entity/Order.java
+++ src/main/java/org/example/ecommerceapi/domain/order/entity/Order.java
@@ -1,13 +1,16 @@
 package org.example.ecommerceapi.domain.order.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.example.ecommerceapi.domain.user.entity.AppUser;
 
+import java.math.BigDecimal;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -16,9 +19,32 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    protected Order() {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser user;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal totalAmount;
+
+    private Order(@NonNull AppUser user, BigDecimal totalAmount) {
+        validateTotalAmount(totalAmount);
+
+        this.user = user;
+        this.totalAmount = totalAmount;
+        this.status = OrderStatus.CREATED;
+    }
+
+    public static Order create(AppUser user, BigDecimal totalAmount) {
+        return new Order(user, totalAmount);
+    }
+
+    private static void validateTotalAmount(BigDecimal totalAmount) {
+        if (totalAmount == null || totalAmount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Total amount must be positive");
+        }
     }
 }
