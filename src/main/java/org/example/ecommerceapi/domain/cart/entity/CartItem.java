427d57c3ac58f92482b49c44e98a607184c8e6b0
lawliet62 <dlwldn62@gmail.com>
2026-06-11 11:22:37 +0900

    CartItem 엔티티에 Cart 및 Product 연관 관계 매핑, 유효성 검사 로직 및 정적 팩토리 메서드 추가

--- src/main/java/org/example/ecommerceapi/domain/cart/entity/CartItem.java
+++ src/main/java/org/example/ecommerceapi/domain/cart/entity/CartItem.java
@@ -1,19 +1,72 @@
 package org.example.ecommerceapi.domain.cart.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.ecommerceapi.domain.product.entity.Product;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"cart_id", "product_id"})
+        }
+)
 public class CartItem {
+
+    private static final int MAX_QUANTITY = 99;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
     private int quantity;
 
-    protected CartItem() {
+    public CartItem(Cart cart, Product product, int quantity) {
+        validateCart(cart);
+        validateProduct(product);
+        validateQuantity(quantity);
+
+        this.cart = cart;
+        this.product = product;
+        this.quantity = quantity;
     }
+
+    public static CartItem create(Cart cart, Product product, int quantity) {
+        return new CartItem(cart, product, quantity);
+    }
+
+    private static void validateCart(Cart cart) {
+        if (cart == null) {
+            throw new IllegalArgumentException("Cart is required");
+        }
+    }
+
+    private static void validateProduct(Product product) {
+        if (product == null) {
+            throw new IllegalArgumentException("Product is required");
+        }
+    }
+
+    private static void validateQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("quantity must be positive");
+        }
+
+        if (quantity > MAX_QUANTITY) {
+            throw new IllegalArgumentException("Quantity must be 99 or less");
+        }
+    }
+
 }
