77ca94156547c66b27e8409a66d61b816d14cfcf
lawliet62 <dlwldn62@gmail.com>
2026-06-11 11:37:20 +0900

    CartItemRepository에 Cart 및 Product를 기준으로 조회하는 메서드 추가

--- src/main/java/org/example/ecommerceapi/domain/cart/repository/CartItemRepository.java
+++ src/main/java/org/example/ecommerceapi/domain/cart/repository/CartItemRepository.java
@@ -1,7 +1,17 @@
 package org.example.ecommerceapi.domain.cart.repository;
 
+import org.example.ecommerceapi.domain.cart.entity.Cart;
 import org.example.ecommerceapi.domain.cart.entity.CartItem;
+import org.example.ecommerceapi.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findAllByCart(Cart cart);
+
+    Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
+
+    Optional<CartItem> findByIdAndCart(Long cartItemId, Cart cart);
 }
