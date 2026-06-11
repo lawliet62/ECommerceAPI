99a021d244ed309787def595896063ffa5e25359
lawliet62 <dlwldn62@gmail.com>
2026-06-11 11:52:07 +0900

    CartItemRequest 필드에 유효성 검사 어노테이션 추가

--- src/main/java/org/example/ecommerceapi/domain/cart/dto/CartItemRequest.java
+++ src/main/java/org/example/ecommerceapi/domain/cart/dto/CartItemRequest.java
@@ -1,7 +1,11 @@
 package org.example.ecommerceapi.domain.cart.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record CartItemRequest(
-        Long productId,
-        int quantity
+        @NotNull @Positive Long productId,
+        @NotNull @Positive @Max(99) Integer quantity
 ) {
 }
