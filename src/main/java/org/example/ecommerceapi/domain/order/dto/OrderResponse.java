a13551f82fece6cf8d0241015387ffbb50b2f923
lawliet62 <dlwldn62@gmail.com>
2026-06-12 09:45:18 +0900

    OrderResponse DTO에 totalAmount 필드 및 items 필드 추가

--- src/main/java/org/example/ecommerceapi/domain/order/dto/OrderResponse.java
+++ src/main/java/org/example/ecommerceapi/domain/order/dto/OrderResponse.java
@@ -2,8 +2,13 @@ package org.example.ecommerceapi.domain.order.dto;
 
 import org.example.ecommerceapi.domain.order.entity.OrderStatus;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public record OrderResponse(
         Long orderId,
-        OrderStatus status
+        OrderStatus status,
+        BigDecimal totalAmount,
+        List<OrderItemResponse> items
 ) {
 }
