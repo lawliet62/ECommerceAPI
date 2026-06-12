98f6d07a56f209b1b93ea5fb1cb1d89205c80da0
lawliet62 <dlwldn62@gmail.com>
2026-06-12 09:35:41 +0900

    OrderRepository에 사용자별 주문 조회 및 단일 주문 조회 메서드 추가

--- src/main/java/org/example/ecommerceapi/domain/order/repository/OrderRepository.java
+++ src/main/java/org/example/ecommerceapi/domain/order/repository/OrderRepository.java
@@ -1,7 +1,14 @@
 package org.example.ecommerceapi.domain.order.repository;
 
 import org.example.ecommerceapi.domain.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAllByUserId(Long userId, Pageable pageable);
+
+    Optional<Order> findByIdAndUserId(Long orderId, Long userId);
 }
