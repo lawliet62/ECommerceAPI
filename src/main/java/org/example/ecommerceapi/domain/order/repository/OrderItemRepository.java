e726ff2a80c55c07858fbba078d73c77e5def24d
lawliet62 <dlwldn62@gmail.com>
2026-06-12 09:35:50 +0900

    OrderItemRepository에 Order별 조회 메서드 추가

--- src/main/java/org/example/ecommerceapi/domain/order/repository/OrderItemRepository.java
+++ src/main/java/org/example/ecommerceapi/domain/order/repository/OrderItemRepository.java
@@ -1,7 +1,11 @@
 package org.example.ecommerceapi.domain.order.repository;
 
+import org.example.ecommerceapi.domain.order.entity.Order;
 import org.example.ecommerceapi.domain.order.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    List<OrderItem> findAllByOrder(Order order);
 }
