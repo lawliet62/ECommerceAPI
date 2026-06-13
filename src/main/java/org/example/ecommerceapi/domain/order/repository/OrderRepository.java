1c3daeb5a6c1426f65a49f443926c941d10cab86
lawliet62 <dlwldn62@gmail.com>
2026-06-13 13:54:21 +0900

    OrderRepository 메서드를 User 객체 기반으로 수정

--- src/main/java/org/example/ecommerceapi/domain/order/repository/OrderRepository.java
+++ src/main/java/org/example/ecommerceapi/domain/order/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package org.example.ecommerceapi.domain.order.repository;
 
 import org.example.ecommerceapi.domain.order.entity.Order;
+import org.example.ecommerceapi.domain.user.entity.AppUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Page<Order> findAllByUserId(Long userId, Pageable pageable);
+    Page<Order> findAllByUser(AppUser user, Pageable pageable);
 
-    Optional<Order> findByIdAndUserId(Long orderId, Long userId);
+    Optional<Order> findByIdAndUser(Long orderId, AppUser user);
 }
