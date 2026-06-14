529624639ab6d445432b3da66987f8c5d72e34ad
lawliet62 <dlwldn62@gmail.com>
2026-06-14 11:03:45 +0900

    PaymentRepository에 사용자와 주문 기반 조회 메서드 추가

--- src/main/java/org/example/ecommerceapi/domain/payment/repository/PaymentRepository.java
+++ src/main/java/org/example/ecommerceapi/domain/payment/repository/PaymentRepository.java
@@ -1,7 +1,18 @@
 package org.example.ecommerceapi.domain.payment.repository;
 
+import org.example.ecommerceapi.domain.order.entity.Order;
 import org.example.ecommerceapi.domain.payment.entity.Payment;
+import org.example.ecommerceapi.domain.user.entity.AppUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Page<Payment> findAllByOrderUser(AppUser user, Pageable pageable);
+
+    Optional<Payment> findByIdAndOrderUser(Long paymentId, AppUser user);
+
+    Optional<Payment> findByOrder(Order order);
 }
