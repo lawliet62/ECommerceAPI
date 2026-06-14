5ae580d8502f88e88f64c36f5d52d54c9d06871b
lawliet62 <dlwldn62@gmail.com>
2026-06-14 11:43:49 +0900

    OrderRepository에서 AppUser 객체 대신 userId를 기반으로 동작하도록 수정 및 관련 메서드 리팩토링

--- src/main/java/org/example/ecommerceapi/domain/order/repository/OrderRepository.java
+++ src/main/java/org/example/ecommerceapi/domain/order/repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package org.example.ecommerceapi.domain.order.repository;
 
 import org.example.ecommerceapi.domain.order.entity.Order;
-import org.example.ecommerceapi.domain.user.entity.AppUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    Page<Order> findAllByUser(AppUser user, Pageable pageable);
+    Page<Order> findAllByUserId(Long userId, Pageable pageable);
 
-    Optional<Order> findByIdAndUser(Long orderId, AppUser user);
+    Optional<Order> findByIdAndUserId(Long orderId, Long userId);
 }
