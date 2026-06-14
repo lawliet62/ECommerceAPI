f2cb1bb0c411df5eec9d8aad5b5ee254231adf16
lawliet62 <dlwldn62@gmail.com>
2026-06-14 11:42:30 +0900

    CartRepository에서 사용자 기반 조회 메서드 제거

--- src/main/java/org/example/ecommerceapi/domain/cart/repository/CartRepository.java
+++ src/main/java/org/example/ecommerceapi/domain/cart/repository/CartRepository.java
@@ -1,14 +1,10 @@
 package org.example.ecommerceapi.domain.cart.repository;
 
 import org.example.ecommerceapi.domain.cart.entity.Cart;
-import org.example.ecommerceapi.domain.user.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserId(Long userId);
-
-    Optional<Cart> findByUser(AppUser user);
-
 }
