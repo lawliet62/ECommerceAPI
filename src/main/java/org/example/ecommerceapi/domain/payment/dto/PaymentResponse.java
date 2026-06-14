2afb1bd0a2fa7cf9e3df2d2e818b68bcf5521110
lawliet62 <dlwldn62@gmail.com>
2026-06-14 11:44:08 +0900

    PaymentResponse에 orderId 및 amount 필드 추가

--- src/main/java/org/example/ecommerceapi/domain/payment/dto/PaymentResponse.java
+++ src/main/java/org/example/ecommerceapi/domain/payment/dto/PaymentResponse.java
@@ -2,8 +2,12 @@ package org.example.ecommerceapi.domain.payment.dto;
 
 import org.example.ecommerceapi.domain.payment.entity.PaymentStatus;
 
+import java.math.BigDecimal;
+
 public record PaymentResponse(
         Long paymentId,
-        PaymentStatus status
+        Long orderId,
+        PaymentStatus status,
+        BigDecimal amount
 ) {
 }
