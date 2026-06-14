400d7762a435c850f9f00e9bfb00e905b4c78740
lawliet62 <dlwldn62@gmail.com>
2026-06-14 11:44:02 +0900

    PaymentRepository에서 AppUser 객체 대신 userId를 기반으로 동작하도록 수정 및 관련 메서드 리팩토링

--- src/main/java/org/example/ecommerceapi/domain/payment/repository/PaymentRepository.java
+++ src/main/java/org/example/ecommerceapi/domain/payment/repository/PaymentRepository.java
@@ -2,7 +2,6 @@ package org.example.ecommerceapi.domain.payment.repository;
 
 import org.example.ecommerceapi.domain.order.entity.Order;
 import org.example.ecommerceapi.domain.payment.entity.Payment;
-import org.example.ecommerceapi.domain.user.entity.AppUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,9 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Page<Payment> findAllByOrderUser(AppUser user, Pageable pageable);
+    Page<Payment> findAllByOrderUserId(Long userId, Pageable pageable);
 
-    Optional<Payment> findByIdAndOrderUser(Long paymentId, AppUser user);
+    Optional<Payment> findByIdAndOrderUserId(Long paymentId, Long userId);
 
     Optional<Payment> findByOrder(Order order);
 
